de5a41312539fd8f18fe6c0b3b9d937e6faa0382
Ena <ena.stankova@yahoo.com>
2016-02-16 01:42:03 +0100

    script for sending emails

---
+++ js/sendmail.js
@@ -0,0 +1,90 @@
+'use strict';
+
+//send email gi pakuva podatocite
+var sendEmail = function (name,from,subject,message) {
+ 
+    var mailApi = "http://mailer-drstankov.rhcloud.com/sendmail";   
+ 
+ 
+    var email = { "name": name, "from": from, "subject": subject, "message": message };
+ 
+}
+
+// sendjson gi praka podatocite
+
+var sendjson = function (url,data) {
+
+  // construct an HTTP request
+  var xhr = new XMLHttpRequest();
+  xhr.open("POST", url, true);
+  xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+
+  // send the collected data as JSON
+  xhr.send(JSON.stringify(data));
+
+  xhr.onloadend = function () {
+    
+      clearform();
+  };
+};
+
+
+//da se brisat podatocite od formata
+
+var clearform = function () {
+
+    var emailForm = document.getElementById("emailForm");
+       
+    emailForm.name.value = "";
+    emailForm.email.value = "";
+    emailForm.subject.value = "";
+    emailForm.message.value = "";
+
+
+};
+
+
+//funkcija za citanje na podatocite od formata
+
+var submitEmailForm = function (e) { 
+    
+    e.preventDefault();
+    
+    var emailForm = document.getElementById("emailForm");
+    
+    sendEmail(
+        emailForm.name.value,
+        emailForm.email.value,
+        emailForm.subject.value,
+        emailForm.message.value
+    );
+}
+
+
+
+
+// form.onsubmit = function (e) {
+//   // stop the regular form submission
+//   e.preventDefault();
+
+//   // collect the form data while iterating over the inputs
+//   var data = {};
+//   for (var i = 0, ii = form.length; i < ii; ++i) {
+//     var input = form[i];
+//     if (input.name) {
+//       data[input.name] = input.value;
+//     }
+//   }
+
+//   // construct an HTTP request
+//   var xhr = new XMLHttpRequest();
+//   xhr.open(form.method, form.action, true);
+//   xhr.setRequestHeader('Content-Type', 'application/json; charset=UTF-8');
+
+//   // send the collected data as JSON
+//   xhr.send(JSON.stringify(data));
+
+//   xhr.onloadend = function () {
+//     // done
+//   };
+// };
